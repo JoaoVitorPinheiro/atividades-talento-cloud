0cfeb7e20358869d882ac5c7ea3d2756cc6af739
JoaoVitorPinheiro <joaovpinheiro8@gmail.com>
2023-12-01 13:12:52 -0300

    comint
    nova atividade

--- Atividade_repeticao_robo_regador.py
+++ Atividade_repeticao_robo_regador.py
@@ -2,10 +2,10 @@
  0,1,4,5. 
 '''
 print('Dia de regar apenas os tomates:')
-
-for robo_regador in range(6):
-  
-    if robo_regador == 2 or robo_regador == 3:
-        continue
-    robo_regador = str(robo_regador)
-    print('Rega a planta ' + robo_regador)
+lista = []
+for robo_regador in lista:
+    print(robo_regador)
+    # if robo_regador == 2 or robo_regador == 3:
+    #     continue
+    # robo_regador = str(robo_regador)
+    # print('Rega a planta ' + robo_regador)
--- atividade_laco_infinito.py
+++ atividade_laco_infinito.py
@@ -1,23 +1,49 @@
-# tecla = input('digite uma letra')
-# while tecla:
-#     if tecla == '':
-
-#         print (f'letra digitada foi {tecla}')
-# else:
-# tecla == 'f'
-# break
-#     print ('fim do laço')
 
 
-tecla = input('Digite uma letra: ')
+rep = 1
 
-while tecla:
-    if tecla == '':
-        print(f'A letra digitada foi {tecla}')
+while rep == 1:
+    soma = int(input('Escolha a operação (1 - Soma, 2 - Subtração, 3 - Multiplicação, 4 - Divisão): '))
+    num1 = int(input('Digite o número 1: '))
+    num2 = int(input('Digite o número 2: '))
+    
+    def calculadora(num1, num2, operacao):
+            if operacao == 1:
+                return num1 + num2
+            elif operacao == 2:
+                return num1 - num2
+            elif operacao == 3:
+                return num1 * num2
+            elif operacao == 4:
+                if num2 != 0:
+                    return num1 / num2
+            else:
+                print("Erro: Divisão por zero.")
+            return 0
+        # else: 
+        # print("Operação não reconhecida. Escolha uma operação válida (1 a 4).")
+        # return 0
+
+    def escrever_operacao(num1, num2, operacao):
+            if operacao == 1:
+                return str(num1) + ' + ' + str(num2) + ' = ' + str(calculadora(num1, num2, operacao))
+            elif operacao == 2:
+                return str(num1) + ' - ' + str(num2) + ' = ' + str(calculadora(num1, num2, operacao))
+            elif operacao == 3:
+                return str(num1) + ' * ' + str(num2) + ' = ' + str(calculadora(num1, num2, operacao))
+            elif operacao == 4:
+                return str(num1) + ' / ' + str(num2) + ' = ' + str(calculadora(num1, num2, operacao))
+            else:
+                return "Operação não reconhecida. Escolha uma operação válida (1 a 4)."
+
+    resultado = escrever_operacao(num1, num2, soma)
+    print(resultado)
+    
+    
+    
+    rep =  int (input('deseja continuar calcular? digire [1]continuar, [2]para sair\n'))
+    if rep == 2:
+        print('fim do laço')
+        break    
     else:
-        if tecla == 'f':
-            break
-        print('Fim do laço')
-
-    # Atualiza a variável `tecla` dentro do loop para evitar um loop infinito
-    tecla = input('Digite uma letra: ')
+         rep = 1

bfd71bfc8622d4aa8c0d1dea7be82754bb23784a
JoaoVitorPinheiro <joaovpinheiro8@gmail.com>
2023-12-04 03:01:27 -0300

    organização
    estou organizando as pastas

---
+++ introducao_html_css/atividade_1_listas.py
@@ -0,0 +1,12 @@
+# Array de produtos de uma loja
+produtos_loja = ["Arroz", "Feijão", "Macarrão", "Óleo de cozinha", "Leite"]
+
+# Array de anos de nascimento de familiares e amigos
+anos_nascimento = [1975, 1995, 2000, 1998, 2016]
+
+# Imprimir os arrays no terminal
+print("Produtos da loja:")
+print(produtos_loja)
+
+print("\nAnos de nascimento de familiares e amigos:")
+print(anos_nascimento)

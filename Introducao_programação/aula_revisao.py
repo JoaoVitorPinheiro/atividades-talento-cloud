d0824647bf0b3a870ac62a30ce3829e2124cc9c4
JOAO VITOR PINHEIRO DOS SANTOS <joaovpinheiro8@gmail.com>
2024-01-09 18:59:48 -0300

    commit dia 09/11/24

--- Introducao_programação/aula_revisao.py
+++ Introducao_programação/aula_revisao.py
@@ -1,10 +1,12 @@
 
-numeroMax = 0
-for numPar in range(0,101,2):
-    print(numPar)
-    numeroMax += numPar
+# numeroMax = 0
+# for numPar in range(0,101,2):
+#     print(numPar)
+#     numeroMax += numPar
 
-print(f'resultado da soma {numeroMax}')
+# print(f'resultado da soma {numeroMax}')
+
+
 
 
 

68954548ac6cae49ea5099b94d53bcfa8f567dd2
JOAO VITOR PINHEIRO DOS SANTOS <joaovpinheiro8@gmail.com>
2024-01-09 18:53:13 -0300

    commit dia 09/11/24

---
+++ Introducao_programação/aula_revisao.py
@@ -0,0 +1,16 @@
+
+numeroMax = 0
+for numPar in range(0,101,2):
+    print(numPar)
+    numeroMax += numPar
+
+print(f'resultado da soma {numeroMax}')
+
+
+
+
+
+
+
+
+
---
+++ pratica_programacao/atividade_4_lista_produtos_novos.py
@@ -0,0 +1,3 @@
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+for produtos_novos in range(len(lista_produtos)):
+    print(f'TEMOS "{lista_produtos[produtos_novos]}" A VENDA!')

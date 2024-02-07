cc903e55bc2653dc770a2dcf9b93edb5c7e9a17c
JoaoVitorPinheiro <143013466+JoaoVitorPinheiro@users.noreply.github.com>
2024-02-07 13:36:13 -0300

    commit das atividades feitas

--- JavaScript/atividades/atividade_inner/script.js
+++ JavaScript/atividades/atividade_inner/script.js
@@ -13,12 +13,13 @@ elementoUL.innerHTML = `
 </ul>
 
 `
+
 let elementoOL = document.querySelector("#lista-ordenada")
 elementoOL.innerHTML = `
 <ol>
-    <li>elemento de lista  ordenada </li>
-    <li>elemento de lista  ordenada </li>
-    <li>elemento de lista  ordenada </li>
+    <li><a href="https://img.freepik.com/fotos-premium/retrato-de-mulher-asiatica-compoe-fantasmacena-de-terror-assustadora-para-fundoconceito-de-festival-de-halloween-poster-de-filmes-fantasma-espirito-raivoso-no-apartamento_44277-22640.jpg">FILMES </a></li>
+    <li><a href="https://st4.depositphotos.com/1022214/21838/i/450/depositphotos_218385028-stock-photo-closeup-frightening-evil-nun-wearing.jpg">nao clique</a></li>
+    <li><a href="https://i.pinimg.com/236x/a4/b0/d2/a4b0d27861271076897ca1a69f2dbafc.jpg">diversao 1</a></li>
 </ol>
 
 `
